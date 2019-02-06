723e8b4cc1c69b966e1769330e76af5c0962c3de
Jack Dabah <jackydabah@gmail.com>
2019-02-06 15:04:32 +0200

    uploading 0kb images

--- app.js
+++ app.js
@@ -26,23 +26,43 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 
-//DB CONNECTION
-app.use(express.static(__dirname + '/public'));
+ //Category and Subcategory for DB and blobnames
+ const category = 'FBIMostWanted';
+ const subcategory = 'Fugitives';
+ const containerName = 'watchlist';
 
-const db = mysql.createConnection({
-   host     : 'trafficintserver.database.windows.net',
-   user     : 'dbmassa@trafficintserver',
-   port     : 1433,
-   password : 'Happy1234',
-   database : 'facedb'
- });
 
+
+
+
+// //LOCAL SQL DB CONNECTION 
+
+// const db = mysql.createConnection({
+//    host     : 'localhost',
+//    user     : 'root',
+//    database : 'facedb'
+//  });
+
+
+//freesqldatabase.com DB CONNECTION
+ const db = mysql.createConnection({
+    host     : 'sql7.freesqldatabase.com',
+    user     : 'sql7277473',
+    password : 'ijHXQEEUik',
+    port     :  3306,
+    database : 'sql7277473'
+  });
  
+
+  const tableName = 'watchlist';
 
  db.connect((err) => {
    if(err) throw err;
    else {
       console.log("MySQL Connected")
+      
+    //   createTable();
+
    }
 })
 
@@ -94,73 +114,69 @@ async function showBlobNames(aborter, containerURL) {
 }
 
 
+async function createTable() {
+    let sql = 'CREATE TABLE '+ tableName + '(id int AUTO_INCREMENT, name VARCHAR(255), category VARCHAR(255), subcategory VARCHAR(255), url VARCHAR(255), PRIMARY KEY(id))';
+        db.query(sql, (err, result) => {
+            if(err) throw err;
+            console.log(result);
+            console.log('Suspects table created...');
+    });
+}
 
-// saveImageToDisk( 'https://www.fbi.gov/wanted/cei/armando-vargas/@@images/image/preview', './test.jpg');
-    
+
 
 const c = new Crawler({
-    callback: function(error, res, done) {
+    callback: async function(error, res, done) {
         if (error) {
             console.log({error})
         } else {
-            console.log(res.$("title").text());
+            // console.log(res.$("title").text());
             const tag = '.portal-type-person div img';
             const images = res.$(tag);
             // images.each((image) => console.log('You have an image: ' + image))
-            images.each(index => {
-                // here you can save the file or save them in an array to download them later
-                var name = images[index].attribs.alt.toString()
-                    .replace(/"/g, '') //remove '"'
-                    .replace(/\s+/g, '-'); //change spaces for '-'
-                saveImageToDisk(images[index].attribs.src, './faces/' + name + '.jpg')
+            // images.each(index => {
+            //     // here you can save the file or save them in an array to download them later
+            //     var name = images[index].attribs.alt.toString()
+            //         .replace(/"/g, '') //remove quotes(")
+            //         .replace(/\s+/g, '-'); //change spaces for (-)
+            //     saveImageToDisk(images[index].attribs.src, './faces/' + name + '.jpg')
 
-                // console.log({
-                //     src: images[index].attribs.src,
-                //     alt: images[index].attribs.alt,
-                // })
-            })
+            //     // console.log({
+            //     //     src: images[index].attribs.src,
+            //     //     alt: images[index].attribs.alt,
+            //     // })
+            // })
+            //TESTING WITH SINGLE UPLOAD
+            var name = images[4].attribs.alt.toString()
+            name = name.replace(/\s+/g, '-').replace(/"/g, ''); 
+            let imgURL = images[4].attribs.src;
+            let localFilePath  = './faces/' + name + '.jpg';
+            console.log("Calling save image to disk with url: " + imgURL + " and filePath : " + localFilePath)
+            await saveImageToDisk(imgURL, localFilePath);
         }
       
     }
 });
 
-// c.queue('https://www.fbi.gov/wanted/fugitives');
+ c.queue('https://www.fbi.gov/wanted/fugitives');
 
-async function createTable() {
-let sql = 'CREATE TABLE suspects(id int AUTO_INCREMENT, name VARCHAR(255), category VARCHAR(255), subcategory VARCHAR(255), url VARCHAR(255), PRIMARY KEY(id))';
-    db.query(sql, (err, result) => {
-        if(err) throw err;
-        console.log(result);
-        console.log('Suspects table created...');
-});
-}
-
-// createTable();
-
-async function addToDB(name, url) {
-    let post = {name:name, category:'FBI Most Wanted', subcategory: 'Fugitives', url:url};
-    let sql = 'INSERT INTO suspects SET ?';
-    let query = db.query(sql, post, (err, result) => {
-        if(err) throw err;
-        console.log(result);
-        console.log('Suspect 1 added...');
-    });
-}
+ 
 
 
-function saveImageToDisk(url, localPath) {
-    var destination = fs.createWriteStream(localPath);
+
+async function saveImageToDisk(url, localFilePath) {
+    var destination = fs.createWriteStream(localFilePath);
     var request = https.get(url, function(response) {
       response.pipe(destination);
     });
     //UPLOAD TO BLOB
-    console.log("sending upload request from: " + localPath)
-    uploadToBlob('a00001', localPath)
+    console.log("Calling uploadToBlob with filePath: " + localFilePath)
+    await uploadToBlob(localFilePath);
 
 }
 
 
-async function uploadToBlob(containerName, localFilePath) {
+async function uploadToBlob(localFilePath) {
 
 
     //ACTIVATE CREDENTIALS AND CONTAINER
@@ -168,61 +184,101 @@ async function uploadToBlob(containerName, localFilePath) {
     const pipeline = StorageURL.newPipeline(credentials);
     const serviceURL = new ServiceURL(`https://${STORAGE_ACCOUNT_NAME}.blob.core.windows.net`, pipeline);
     const containerURL = ContainerURL.fromServiceURL(serviceURL, containerName);  
+    console.log("Container URL: " + containerURL.url)
     const aborter = Aborter.timeout(30 * ONE_MINUTE);
 
-    //Create container if exists catch error.
-    try{
-        await containerURL.create(aborter);
-        console.log(`Container: "${containerName}" is created`);
-    }
-    catch(err) {
-        console.log(err.message);
-    }
+    // Create container if exists catch error.
+    // try{
+    //     await containerURL.create(aborter);
+    //     console.log(`Container: "${containerName}" is created`);
+    // }
+    // catch(err) {
+    //     console.log(err.message);
+    // }
 
     //UPLOAD IMAGE
-    var fileName = localFilePath.substring(localFilePath.lastIndexOf('/') +1);
-    // console.log("Creating blob from: " + localFilePath + " with name: " + fileName)
-    await uploadImage(aborter, containerURL, localFilePath, fileName)
+    var blobName = localFilePath.substring(localFilePath.lastIndexOf('/') + 1 );
+    // fileName = fileName.replace(/\s/gi, '-')
+    console.log("Calling upload image with filePath: " + localFilePath + " and name: " + blobName)
+    await uploadImage(aborter, containerURL, localFilePath, blobName)
 
 
 }
 
-
+//UPLOAD IMAGE NO-STREAM
 async function uploadImage(aborter, containerURL, filePath, blobName) {
+    try {
+        filePath = path.resolve(filePath);
 
-    filePath = path.resolve(filePath);
+        // var fullBlobName = category + "/" + subcategory + "/" + blobName;
+        // randomBlobName = 
+        console.log("Getting blockBlorURL with containerURL: " + containerURL.url 
+                            + " and blobName: " + blobName)
+        const blockBlobURL = BlockBlobURL.fromContainerURL(containerURL, blobName);
+        console.log("Blob created, URL is: " + JSON.stringify(blockBlobURL.url));
 
-    const blockBlobURL = BlockBlobURL.fromContainerURL(containerURL, blobName);
-    console.log("Blob created, URL is: " + JSON.stringify(blockBlobURL.url));
+        // addToDB(blobName, blockBlobURL.url);
+        
+        console.log("Calling uploadFileToBlockBlob with filePath: " + filePath + 
+                    " and blobURL: "+blockBlobURL.url)
+        return await uploadFileToBlockBlob(aborter, filePath, blockBlobURL);
+    }
+    catch(err) {
+        console.log(err)
+    }
+}
 
-    const stream = fs.createReadStream(filePath, {
-      highWaterMark: FOUR_MEGABYTES,
+//UPLOAD IMAGE USING STREAM
+// async function uploadImage(aborter, containerURL, filePath, blobName) {
+//     try{ 
+//     filePath = path.resolve(filePath);
+
+//     // var fullBlobName = category + "/" + subcategory + "/" + blobName;
+//     const blockBlobURL = BlockBlobURL.fromContainerURL(containerURL, blobName);
+//     console.log("Blob created, URL is: " + JSON.stringify(blockBlobURL.url));
+
+//     const stream = fs.createReadStream(filePath, {
+//       highWaterMark: FOUR_MEGABYTES,
+//     });
+
+//     const uploadOptions = {
+//         bufferSize: FOUR_MEGABYTES,
+//         maxBuffers: 5,
+//     };
+
+
+//     // //SAVE URL TO DB
+//     // addToDB(blobName, blockBlobURL.url);
+
+//     console.log("JERE")
+
+//     return await uploadStreamToBlockBlob(
+//                     aborter, 
+//                     stream, 
+//                     blockBlobURL, 
+//                     uploadOptions.bufferSize, 
+//                     uploadOptions.maxBuffers);
+//   } catch (err) {console.log(err)}
+// }
+
+
+async function addToDB(name, url) {
+    //REGULAR SQL SYNTAX
+
+
+    let post = {name:name, category: category, subcategory: subcategory, url:url};
+    let sql = 'INSERT INTO ' + tableName + ' SET ?';
+    let query = db.query(sql, post, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        console.log('Suspect ' +  name + ' added...');
     });
 
-    const uploadOptions = {
-        bufferSize: FOUR_MEGABYTES,
-        maxBuffers: 5,
-    };
-
-    //SAVE URL TO DB
-    addToDB(blobName, blockBlobURL.url);
-
-    
-
-    return await uploadStreamToBlockBlob(
-                    aborter, 
-                    stream, 
-                    blockBlobURL, 
-                    uploadOptions.bufferSize, 
-                    uploadOptions.maxBuffers);
 }
 
 
-var downloadedContent;
-app.get('/', (req,res) => {
-    res.render('index', {image:downloadedContent})
-})
 
-app.listen(process.env.PORT, process.env.IP, (req,res) => {
-    console.log("Azure-blobs server started!!!")
-})
+
+// app.listen(process.env.PORT, process.env.IP, (req,res) => {
+//     console.log("Azure-blobs server started!!!")
+// })
